b28e13e6d075042826651a549c7226e03ea01f9d
Maxwell Penders <max.penders@gmail.com>
2021-12-18 16:15:06 -0500

    Started work on the actual code

--- brawlhalla.py
+++ brawlhalla.py
@@ -1,30 +1,17 @@
+#This game was created by Maxwell Penders and Jett Rosner
+
+### Imports ###
+import sys
 import pygame
-from pygame.locals import *
+
+### Constants ###
+
+window_width = 600
+window_height = 400
+
+### Main Code ###
+# Initializes all pygame modules
 pygame.init()
-screen = pygame.display.set_mode((1000, 1000))
-running = True
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-BLACK = (0, 0, 0)
-GRAY = (127, 127, 127)
-WHITE = (255, 255, 255)
-YELLOW = (255, 255, 0)
-CYAN = (0, 255, 255)
-MAGENTA = (255, 0, 255)
-background = GRAY
-key_dict = {K_k:BLACK, K_r:RED, K_g:GREEN, K_b:BLUE,
-    K_y:YELLOW, K_c:CYAN, K_m:MAGENTA, K_w:WHITE}
-while running:
-    for event in pygame.event.get():
-        if event.type == KEYDOWN:
-            if event.key in key_dict:
-                background = key_dict[event.key]
-                screen.fill(background)
-                pygame.display.update()
-        if event.type == pygame.QUIT:
-            running = False
 
-pygame.quit()
-
-#comment to show jett something
+# Sets our display window
+pygame.display.set_mode(window_width, window_height)
